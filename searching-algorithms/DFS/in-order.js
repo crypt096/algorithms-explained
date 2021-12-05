ebd4a7e82bed4cac596ec2594a435a68d9c132f0
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2021-12-05 12:54:23 +0100

    Add fix.

--- searching-algorithms/DFS/in-order.js
+++ searching-algorithms/DFS/in-order.js
@@ -1,12 +1,12 @@
 const depthFirstSearch = (node, list) => {
   if (node.left) {
-    depthFirstSearch(node.left, data);
+    depthFirstSearch(node.left, list);
   }
-  
+
   list.push(node.value);
 
   if (node.right) {
-    depthFirstSearch(node.right, data);
+    depthFirstSearch(node.right, list);
   }
 
   return list;
